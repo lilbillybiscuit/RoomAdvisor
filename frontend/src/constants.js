b333610066718804443d2cf46c1a46ab7d8d0360
ken <57498837+kentng01@users.noreply.github.com>
2023-04-04 21:00:30 -0400

    Initial alterations.

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,7 @@
-export const dserverIp = "https://server.abrahammensah.com";
-export const dclientIp = "https://abrahammensah.com";
+// the following will be auto changed during deployment:
+// export const dserverIp = "https://server.abrahammensah.com";
+// export const dclientIp = "https://abrahammensah.com";
+
 export const serverIp = "http://localhost:4000";
 export const clientIp = "http://localhost:3000";
 export const cryptoKey = "1234";
