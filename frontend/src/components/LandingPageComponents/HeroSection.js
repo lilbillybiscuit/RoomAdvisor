4d3092444533e07f2b9c641196b939ca301f6c53
kenan-erol <kenan.erol@yale.edu>
2023-04-14 17:16:47 -0400

    Added about button, fixed paragraph

--- frontend/src/components/LandingPageComponents/HeroSection.js
+++ frontend/src/components/LandingPageComponents/HeroSection.js
@@ -9,43 +9,51 @@ function HeroSection({ isLoggedIn }) {
     <div className="hero-container">
       <h1>Choosing a room just got easier</h1>
 
-      <p>
+      {/* <p>
         Room Advisor is a website where Yale students can candidly rate and
         review residential college rooms. We seek to improve the Yale housing
         experiences by empowering Yale students with information to help them
         find living spaces they love.
-      </p>
+      </p> */}
 
-      <div className="mobile-para">
+      <ul className="intro">
+        <li>⭐️ Rate and review Yale dorms</li>
+        <li>👯 Communicate which rooms you want</li>
+        <li>🤔 Check noise, sleep times, cleanliness, etc.</li>
+        <li>😎 Coordinate with friends! (maybe)</li>
+      </ul>
+
+      {/* <div className="mobile-para">
         <div>
           Room Advisor is a website where Yale students can candidly rate and
           review residential college rooms. We seek to improve the Yale housing
           experiences by empowering Yale students with information to help them
           find living spaces they love.
         </div>
-      </div>
+      </div> */}
 
       <div className="lg-screen-button">
         <Button buttonStyle="btn--primary" buttonSize="btn--medium">
           <LoginComponent isLoggedIn={isLoggedIn} />
         </Button>
+        <Button buttonStyle="btn--secondary" buttonSize="btn--medium">
+          <Link to="/about" style={{ color: "white" }}>
+            About
+          </Link>
+        </Button>
       </div>
 
       <div className="sm-screen-buttons">
-        <div className="sm-screen-button">
-          <Button buttonStyle="btn--primary" buttonSize="btn--medium">
-            <LoginComponent isLoggedIn={isLoggedIn} />
-          </Button>
-        </div>
-
-        <div className="sm-screen-button">
-          <Button buttonStyle="btn--primary" buttonSize="btn--medium">
-            <Link to="/about" style={{ color: "white" }}>
-              About
-            </Link>
-          </Button>
-        </div>
+        <Button buttonStyle="btn--primary" buttonSize="btn--medium">
+          <LoginComponent isLoggedIn={isLoggedIn} />
+        </Button>
+        <Button buttonStyle="btn--secondary" buttonSize="btn--medium">
+          <Link to="/about" style={{ color: "white" }}>
+            About
+          </Link>
+        </Button>
       </div>
+      
     </div>
   );
 }
