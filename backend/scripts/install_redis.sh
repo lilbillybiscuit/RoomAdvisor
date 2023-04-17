d4da234de57a42e5acad188e80e1dadf5f793e60
lilbillybiscuit <qianbill2014@outlook.com>
2023-04-17 14:08:55 -0400

    Switch away from redis, and polish Postgres scripts

--- backend/scripts/install_redis.sh
+++ backend/scripts/install_redis.sh
@@ -28,7 +28,7 @@ then
 
     sudo sed -i "s/bind 127.0.0.1/bind $redis_ip 127.0.0.1/g" /etc/redis/redis.conf
     sudo sed -i "s/protected-mode yes/protected-mode no/g" /etc/redis/redis.conf
-
+    sudo systemctl restart redis-server.service
 
 else
     echo "Redis is already installed."
@@ -36,8 +36,11 @@ fi
 
 # Configure Redis to require authentication, if the --no-replace-password flag is provided, the script will skip the password creation if it already exists.
 
-# get the current redis password from the redis configuration
+# get the current redis password from the redis configuration.
 CURRENT_REDIS_PASSWORD=$(sudo grep -oP '(?<=^requirepass ).*' /etc/redis/redis.conf)
+
+# check if redis password already exists using CURRENT_REDIS_PASSWORD using redis-cli config get requirepass
+PASS=$
 
 # check if redis password already exists using CURRENT_REDIS_PASSWORD to authenticate
 if [ "$(redis-cli -a $CURRENT_REDIS_PASSWORD config get requirepass  | grep -c "requirepass")" = '1' ]
@@ -86,52 +89,6 @@ else
     echo "Redis configuration updated to require authentication."
 fi
 
-#
-## check if redis password already exists
-#if [ "$(redis-cli config get requirepass  | grep -c "requirepass")" = '1' ]
-#then
-#    # check if --no-replace-password flag is provided, if so, skip password creation
-#    if [ "$3" = "--no-replace-password" ]
-#    then
-#        echo "Redis password already exists. Skipping password creation."
-#    else
-#        # if no password provided, set a random password
-#        if [ -z "$2" ]
-#        then
-#            REDIS_PASSWORD=$(openssl rand -hex 16)
-#            echo "Randomly generated redis password: $REDIS_PASSWORD"
-#        else
-#            REDIS_PASSWORD=$2
-#        fi
-#
-#        # set the password in the redis configuration
-#        redis-cli config set requirepass $REDIS_PASSWORD
-#        sudo systemctl restart redis-server.service
-#
-#        # update the redis configuration to require authentication
-#        sudo sed -i "s/# requirepass foobared/requirepass $REDIS_PASSWORD/g" /etc/redis/redis.conf
-#        sudo systemctl restart redis-server.service
-#        echo "Redis configuration updated to require authentication."
-#    fi
-#else
-#    # if no password provided, set a random password
-#    if [ -z "$2" ]
-#    then
-#        REDIS_PASSWORD=$(openssl rand -hex 16)
-#        echo "Randomly generated redis password: $REDIS_PASSWORD"
-#    else
-#        REDIS_PASSWORD=$2
-#    fi
-#
-#    # set the password in the redis configuration
-#    redis-cli config set requirepass $REDIS_PASSWORD
-#    sudo systemctl restart redis-server.service
-#
-#    # update the redis configuration to require authentication
-#    sudo sed -i "s/# requirepass foobared/requirepass $REDIS_PASSWORD/g" /etc/redis/redis.conf
-#    sudo systemctl restart redis-server.service
-#    echo "Redis configuration updated to require authentication."
-#fi
 
 echo "Redis installation complete."
 exit 0
