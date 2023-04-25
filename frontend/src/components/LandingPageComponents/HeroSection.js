cc203626171ed6208e884315d681dd5a3f8161ee
lilbillybiscuit <qianbill2014@outlook.com>
2023-04-24 23:12:30 -0400

    Merge tony's new homepage

--- frontend/src/components/LandingPageComponents/HeroSection.js
+++ frontend/src/components/LandingPageComponents/HeroSection.js
@@ -1,53 +1,112 @@
 import "./HeroSection.css";
-import React from "react";
-import { Button } from "src/components/Button/Button";
+import React, { ReactPropTypes } from "react";
 import LoginComponent from "./Login";
 import { Link } from "react-router-dom";
+import logo from "src/images/logo.png";
 
-function HeroSection() {
-  return (
-    <div className="hero-container">
-      <h1>Choosing a room just got easier</h1>
+const messages = [
+    {
+        msg: "Rate and review Yale dorms",
+    },
+    {
+        msg: "Communicate which rooms you want",
+    },
+    {
+        msg: "Check noise, steep times, cleanliness, etc.",
+    },
+    {
+        msg: "Coordinate with friends!",
+    },
+];
 
-      <p>
-        Room Advisor is a website where Yale students can candidly rate and
-        review residential college rooms. We seek to improve the Yale housing
-        experiences by empowering Yale students with information to help them
-        find living spaces they love.
-      </p>
+export default function HeroSection(props) {
+    return (
+        <div className="hero-container">
+            <div className="landing-container">
+                <img
+                    src={logo}
+                    alt="room-advisor-logo"
+                    style={{
+                        position: "relative",
+                        height: "100px",
+                        width: "220px",
+                        zIndex: "5",
+                        objectFit: "contain",
+                    }}
+                />
 
-      <div className="mobile-para">
-        <div>
-          Room Advisor is a website where Yale students can candidly rate and
-          review residential college rooms. We seek to improve the Yale housing
-          experiences by empowering Yale students with information to help them
-          find living spaces they love.
+                <ul className="landing-messages-container">
+                    {messages.map((msg) => (
+                        <li className="circle-checkmark">{msg.msg}</li>
+                    ))}
+                </ul>
+
+                <div className="landing-buttons-wrapper">
+                    <button
+                        className="landing-button-login"
+                        style={{ backgroundColor: "#0053c5" }}
+                    >
+                        <LoginComponent isLoggedIn={props.isLoggedIn} />
+                    </button>
+
+                    <Link to="/about" style={{ color: "white" }}>
+                        <button
+                            className="landing-button-about"
+                            style={{ backgroundColor: "#888888" }}
+                        >
+                            About
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className="lg-screen-button">
-        <Button buttonStyle="btn--primary" buttonSize="btn--medium">
-          <LoginComponent/>
-        </Button>
-      </div>
-
-      <div className="sm-screen-buttons">
-        <div className="sm-screen-button">
-          <Button buttonStyle="btn--primary" buttonSize="btn--medium">
-            <LoginComponent/>
-          </Button>
-        </div>
-
-        <div className="sm-screen-button">
-          <Button buttonStyle="btn--primary" buttonSize="btn--medium">
-            <Link to="/about" style={{ color: "white" }}>
-              About
-            </Link>
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
-export default HeroSection;
+// function HeroSection({ isLoggedIn }) {
+//   return (
+//     <div className="hero-container">
+//       <h1>Choosing a room just got easier</h1>
+
+//       <p>
+//         Room Advisor is a website where Yale students can candidly rate and
+//         review residential college rooms. We seek to improve the Yale housing
+//         experiences by empowering Yale students with information to help them
+//         find living spaces they love.
+//       </p>
+
+//       <div className="mobile-para">
+//         <div>
+//           Room Advisor is a website where Yale students can candidly rate and
+//           review residential college rooms. We seek to improve the Yale housing
+//           experiences by empowering Yale students with information to help them
+//           find living spaces they love.
+//         </div>
+//       </div>
+
+//       <div className="lg-screen-button">
+//         <Button buttonStyle="btn--primary" buttonSize="btn--medium">
+//           <LoginComponent isLoggedIn={isLoggedIn} />
+//         </Button>
+//       </div>
+
+//       <div className="sm-screen-buttons">
+//         <div className="sm-screen-button">
+//           <Button buttonStyle="btn--primary" buttonSize="btn--medium">
+//             <LoginComponent isLoggedIn={isLoggedIn} />
+//           </Button>
+//         </div>
+
+//         <div className="sm-screen-button">
+//           <Button buttonStyle="btn--primary" buttonSize="btn--medium">
+//             <Link to="/about" style={{ color: "white" }}>
+//               About
+//             </Link>
+//           </Button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default HeroSection;
